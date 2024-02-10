ebd19c65e2b00fc1f20b9c0e99d943a5a2e5edfe
Andrii Iurchenko <spree92spree92@gmail.com>
2024-02-10 11:05:23 +0200

    adjusted question headline

--- src/components/Questions/QuestionHeadline.jsx
+++ src/components/Questions/QuestionHeadline.jsx
@@ -1,10 +1,33 @@
 import React from "react";
 
 export default function QuestionHeadline({ question, quote }) {
+  const renderDialogue = () => {
+    if (typeof quote === "string" || quote.length === 1) {
+      return <p className="mt-[10px] font-semibold italic">"{quote}"</p>;
+    } else {
+      return (
+        <ul className="flex-center items-start">
+          {quote.map((item, index) => {
+            const isCharacter = item.substring("Character");
+
+            return (
+              <li
+                key={index}
+                className={isCharacter ? "font-semibold" : "italic"}
+              >
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+      );
+    }
+  };
+
   return (
     <>
       <h2 className="mt-[20px] font-bold">{question}</h2>
-      <p className="mt-[10px] font-semibold italic">"{quote}"</p>
+      {renderDialogue()}
     </>
   );
 }
